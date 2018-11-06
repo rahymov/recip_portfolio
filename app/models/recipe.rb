2d822d1a8cb7ff1df9fd810bb84a4427852ad9b1
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-11-05 21:41:11 -0500

    create recipe_category model&migration and associations between category, recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :recipe_categories
+  has_many :categories, through: :recipe_categories
   validates :title, :description, presence: true, uniqueness: true
 end

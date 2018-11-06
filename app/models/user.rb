9a47526d573a2b7a443c11d700c7bd6f68419083
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-11-05 22:19:19 -0500

    add recipe index new edit views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
-  
+  has_many :recipes
 end

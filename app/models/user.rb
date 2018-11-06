484437701ac6134472321342ab56d66c1a5eb9ff
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-11-05 21:23:10 -0500

    design signin page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :omniauthable,
          :recoverable, :rememberable, :validatable, :confirmable, :trackable
+  
 end

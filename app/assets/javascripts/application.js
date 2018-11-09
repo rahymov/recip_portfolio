c7f463a849f5d010e91c475ed7b1b5f9d0e820e0
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-11-08 20:52:08 -0500

    add styling to application.scss, create footer partial

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,10 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require jquery3
 //= require popper
+//= require jquery
+//= require cocoon
 //= require bootstrap-sprockets
+//= require turbolinks
 //= require_tree .

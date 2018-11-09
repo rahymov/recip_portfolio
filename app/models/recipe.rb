122861eb3b5052659df05dc563be20551018ead4
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-11-08 20:50:53 -0500

    accept nested form, image allow nil, ingredient&direction associations with recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,6 +3,18 @@ class Recipe < ApplicationRecord
   has_many :recipe_categories
   has_many :categories, through: :recipe_categories
   validates :title, :description, presence: true, uniqueness: true
+
   has_one_attached :image
+  delegate :filename, to: :image, allow_nil: true
+
+  has_many :directions
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients,
+																reject_if: proc {|attributes| attributes['name'].blank?},
+																allow_destroy: true
+	accepts_nested_attributes_for :directions,
+																reject_if: proc {|attributes| attributes['step'].blank?},
+																allow_destroy: true
+
   scope :order_and_categories, -> { order("title").includes(:categories)}
 end
